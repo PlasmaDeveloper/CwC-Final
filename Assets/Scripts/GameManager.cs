a8d3305ff05037626b564c58b631430ff3b503a2
Plasma <50627025+PlasmaDeveloper@users.noreply.github.com>
2022-04-26 12:57:04 +0200

    added chessboard to script
    added the chesboard gameobjects to the gamemanager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,14 @@ public class GameManager : MonoBehaviour
 
     //values
     [SerializeField] bool piceIsSelected;
+    [SerializeField] GameObject[] line1;
+    [SerializeField] GameObject[] line2;
+    [SerializeField] GameObject[] line3;
+    [SerializeField] GameObject[] line4;
+    [SerializeField] GameObject[] line5;
+    [SerializeField] GameObject[] line6;
+    [SerializeField] GameObject[] line7;
+    [SerializeField] GameObject[] line8;
 
     // Start is called before the first frame update
     void Start()
