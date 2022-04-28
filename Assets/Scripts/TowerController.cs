1a57c44628cbf962fc713da36af1f96fac419c70
Plasma <50627025+PlasmaDeveloper@users.noreply.github.com>
2022-04-28 16:32:13 +0200

    tower controller
    Added a tower controller, but it is completely bugged - have to fix this later

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerController : MonoBehaviour
+public class TowerController : ChessPieceController
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        TowerSetup();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TowerSetup()
+    {
+        ObjectSetup();
+
+        defaultFieldsToMove.Add(1);
+        defaultFieldsToMove.Add(2);
+        defaultFieldsToMove.Add(3);
+        defaultFieldsToMove.Add(4);
+        defaultFieldsToMove.Add(5);
+        defaultFieldsToMove.Add(6);
+        defaultFieldsToMove.Add(7);
+        defaultFieldsToMove.Add(8);
+
+        moveDirections.Add('l');
+        moveDirections.Add('r');
+        moveDirections.Add('b');
     }
 }
